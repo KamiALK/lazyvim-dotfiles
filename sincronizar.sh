f69bfa9f5d222f8ef34d99e22bf2881092205cce
kamilo <andresyalca@gmail.com>
2025-10-18 18:48:04 -0500

    actualizacion

--- sincronizar.sh
+++ sincronizar.sh
@@ -1,6 +1,7 @@
 echo " eliminando carrpeta  vieja "
 rm -rf lua # eliminar el enlace simbólico
 cp -r ~/.config/nvim/lua ./lua
+echo "haciendo commit"
 git add .
 git commit -m "actualizacion"
 git push origin main
